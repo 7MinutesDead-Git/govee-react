5426d9e7e6b15bc1e395e65c96cfba967f81be6e
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-28 22:07:09 -0500

    Add interfaces for contracts of API calls

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,46 @@
+enum h6003SupportedCommands {
+    turn = "turn",
+    brightness = "brightness",
+    color = "color",
+    colorTem = "colorTem"
+}
+
+export interface goveeCommandRequest {
+    device: string,
+    model: string,
+    cmd: {
+        name: string,
+        value: string
+    }
+}
+
+export interface goveeCommandResponse {
+    code: number,
+    message: string,
+    data: {}
+}
+
+export interface goveeDevicesResponse {
+    code: number,
+    message: string,
+    data: {
+        devices: goveeDevice[]
+    }
+}
+
+export interface goveeDevice {
+    device: string,
+    model: string,
+    deviceName: string,
+    controllable: boolean,
+    retrievable: boolean,
+    supportCmds: string[],
+    properties: {
+        colorTem: {
+            range: {
+                min: number,
+                max: number
+            }
+        }
+    }
+}
