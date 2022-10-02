e3c68f6e80b831e5c642b0c9ddabe042d09dbdd9
7MinutesDead-Git <7minutesdead@gmail.com>
2022-10-01 19:16:31 -0500

    Add new features: Color changing, current state of light bulbs (...) - Fetch additional data from current status (state) API, including current bulb online status, power status, color, and brightness. - Indicate fetch/refresh status when button is clicked. - Add helper function to merge data from initial API request with subsequent state requests per fetched light (needs a refactor to be more readable).

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { MantineProvider, createStyles, Text, Button, Stack } from "@mantine/core"
+import { MantineProvider, createStyles, Text, Button, Stack, Divider } from "@mantine/core"
 import { theme } from "./theme"
 import { TableOfLights } from "./components/ProgressTable"
-import { useEffect, useState } from "react"
-import { goveeDevice } from "./interfaces/interfaces";
+import {MouseEventHandler, SyntheticEvent, useEffect, useState} from "react"
+import {goveeDevice, goveeDevicesMap, goveeDeviceWithState, goveeStateResponse} from "./interfaces/interfaces";
 
-const initialDevices: goveeDevice[] = []
+const initialDevices: goveeDeviceWithState[] = []
 const devicesURL = "http://localhost:3001/devices"
+const stateURL = "http://localhost:3001/devices/state"
+const cyberPink = "#ff0051"
+const oceanGreen = "#04E184"
+const backgroundColor = "#1A1B1E"
+const backgroundFlashColor = "#2A2B2E"
 
 const useStyles = createStyles((theme) => ({
     hidden: {
         opacity: 0,
-        transition: 'opacity 200ms ease-in-out',
+        transition: 'opacity 200ms ease-in-out'
     },
     visible: {
         opacity: 1,
@@ -19,14 +24,10 @@ const useStyles = createStyles((theme) => ({
 }))
 
 export default function App() {
-    const cyberPink = "#ff0051"
-    const oceanGreen = "#04E184"
-    const backgroundColor = "#1A1B1E"
-    const backgroundFlashColor = "#2A2B2E"
-
     const [connectedLights, setConnectedLights] = useState(initialDevices)
-    const [buttonColor, setButtonColor] = useState(cyberPink)
+    const [buttonColor, setButtonColor] = useState(oceanGreen)
     const [rateLimit, setRateLimit] = useState(0)
+    const [buttonText, setButtonText] = useState("Connect to 7MD Lights")
     const { classes } = useStyles()
 
     async function startRateLimitTimer(responseBody: any) {
@@ -34,15 +35,20 @@ export default function App() {
     }
 
 
-    async function handleButtonClick() {
-        if (buttonColor === cyberPink) {
-            setButtonColor(oceanGreen)
+    async function handleButtonClick(event: SyntheticEvent) {
+        let target = event.target as HTMLElement
+
+        if (target.tagName === "SPAN") {
+            // The Mantine button text is a span, wrapped in a div, sat inside button.
+            // TODO: Refactor this to be less dumb.
+            target = target.parentElement!.parentElement!
         }
-        else {
-            setButtonColor(cyberPink)
-        }
+        target.classList.add("fetching")
+        setButtonText("Connecting...")
         await getStatusOfLights()
+        target.classList.remove("fetching")
         flashBackground()
+        setButtonText("Refresh")
     }
 
     function flashBackground() {
@@ -55,22 +61,57 @@ export default function App() {
 
     async function getStatusOfLights() {
         const response = await fetch(devicesURL);
-        const onlineDevices: goveeDevice[] = await response.json();
-        setConnectedLights(onlineDevices);
+        const onlineDevices: goveeDevice[] = await response.json()
+        const completeDevices: goveeDevicesMap = {}
+        const deviceStatsPromises = []
+        const results = []
+
+        for (const device of onlineDevices) {
+            completeDevices[device.device] = {
+                id: device.device,
+                details: {...device},
+                status: {
+                    brightness: 0,
+                    color: { b: 0, g: 0, r: 0 },
+                    online: false,
+                    powerState: "off"
+                }
+            }
+            const deviceStatsPromise = fetch(`${stateURL}?device=${device.device}&model=${device.model}`)
+            deviceStatsPromises.push(deviceStatsPromise)
+        }
+        const deviceStatsResponses = await Promise.allSettled(deviceStatsPromises)
+
+        for (const deviceStatsResponse of deviceStatsResponses) {
+            if (deviceStatsResponse.status === "fulfilled") {
+                const deviceFetchedStats: goveeStateResponse = await deviceStatsResponse.value.json()
+                const extractedStatusProperties = {}
+                for (const propertyObject of deviceFetchedStats.data.properties) {
+                    const propertyKey = Object.keys(propertyObject)[0]
+                    // TODO: Properly type this temporary object.
+                    // @ts-ignore
+                    extractedStatusProperties[propertyKey] = propertyObject[propertyKey]
+                }
+                // TODO: Properly type this temporary object.
+                // @ts-ignore
+                completeDevices[deviceFetchedStats.data.device].status = extractedStatusProperties
+            }
+        }
+        for (const device in completeDevices) {
+            results.push(completeDevices[device])
+        }
+        setConnectedLights(results)
     }
 
     return (
         <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-            <Stack align="center" mt={50}>
-                <Text size="xl" weight={500}>
-                    THE TIME IS NOW!
-                </Text>
-                <Button
-                    color={buttonColor}
-                    onClick={handleButtonClick}
-                    className="btn-lights-status">
-                    Check For Lights
+            <Stack align="center" justify="center" spacing="xl">
+                <Button color={buttonColor}
+                        onClick={handleButtonClick}
+                        className="btn-lights-status">
+                    {buttonText}
                 </Button>
+                <Divider my="sm" variant="dashed" />
                 <TableOfLights data={connectedLights}/>
             </Stack>
         </MantineProvider>
