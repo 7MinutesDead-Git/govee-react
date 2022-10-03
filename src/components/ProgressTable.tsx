0d571ed8d6790a5a5656f68b2351bb329480c401
7MinutesDead-Git <7minutesdead@gmail.com>
2022-10-03 02:18:18 -0500

    Create Row component for managing state (...) - Add row style objects for component styling. - Move state from TableOfLights to new Row component. - Add state for tracking illumination, color and rate limited state. - Add visual feedback indicating successful or unsuccessful fetch requests. - Add visual feedback when rate limited. - Add Light Bulb thumb icon component to brightness slider.

--- src/components/ProgressTable.tsx
+++ src/components/ProgressTable.tsx
@@ -1,6 +1,17 @@
-import { createStyles, Table, Badge, Slider, ColorPicker, Text } from '@mantine/core'
-import {goveeDevice, goveeDevicesResponse, goveeDeviceWithState} from '../interfaces/interfaces'
-import {useEffect, useRef, useState} from 'react'
+import { createStyles, Table, Slider, ColorPicker, Text } from '@mantine/core'
+import { goveeDeviceNameOnly, goveeDeviceWithState,} from '../interfaces/interfaces'
+import { useEffect, useRef, useState} from 'react'
+import { BadgeNetworkStatus, BadgeIlluminationStatus } from "./Badges"
+import { BulbIcon } from "./Icons"
+
+interface LightTableRowProps {
+    light: goveeDeviceWithState
+}
+
+interface LightTableProps {
+    lights: goveeDeviceWithState[]
+}
+
 
 const useStyles = createStyles((theme) => ({
     progressBar: {
@@ -36,11 +47,26 @@ const useStyles = createStyles((theme) => ({
     visible: {
         opacity: 1,
         transition: 'opacity 1s ease-in-out',
-    },
-    "control-surface": {
-        padding: "1.5rem 0"
     }
 }));
+
+const rowStyles = {
+    controlSurface: {
+        padding: "1.5rem 0"
+    },
+    fetchSuccess: {
+        animation: "success 0.5s ease-in-out",
+        animationFillMode: "forwards",
+    },
+    fetchFailure: {
+        animation: "failure 2s ease-in-out infinite",
+        animationFillMode: "forwards",
+    },
+    fetchReset: {
+        animation: "",
+        animationFillMode: ""
+    }
+}
 
 
 function rgbToHex(r: number, g: number, b: number) {
@@ -51,9 +77,53 @@ function rgbToHex(r: number, g: number, b: number) {
 }
 
 
-export function TableOfLights(props: any) {
-    const [ brightness, setBrightness ] = useState(0)
-    const { classes, theme } = useStyles();
+export function TableOfLights(props: LightTableProps) {
+    const rowsSorted = props.lights.sort((a: goveeDeviceNameOnly, b: goveeDeviceNameOnly) => {
+        return a.details.deviceName.toLowerCase().localeCompare(b.details.deviceName.toLowerCase())
+    })
+
+    const rows = rowsSorted.map((light: goveeDeviceWithState) => {
+        return (<LightTableRow light={light} key={light.id}/>)
+    })
+
+    return (
+        <Table sx={{ maxWidth: '1000px' }}
+               verticalSpacing="xs"
+               striped={true}
+               horizontalSpacing="xs"
+               highlightOnHover={true}
+               className="table-lights-status">
+            <thead>
+                <tr>
+                    <th>Light Location</th>
+                    <th>Model</th>
+                    <th>Status</th>
+                    <th>Color and Brightness</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </Table>
+    )
+}
+
+
+const LightTableRow = (props: LightTableRowProps) => {
+    const { light } = props
+    const blackColor = {r: 0, g: 0, b: 0}
+    const makingLight = light.status.powerState === "on"
+        && light.status.brightness > 0
+        && light.status.color !== blackColor
+
+    const { classes, theme } = useStyles()
+
+    const [ brightness, setBrightness ] = useState(light.status.brightness)
+    const [ online, setOnline ] = useState(light.status.online)
+    const [ rateLimited, setRateLimited ] = useState(false)
+    const [ illuminating, setIlluminating ] = useState(makingLight)
+    const [ color, setColor ] = useState("")
+    const [rowFetchStyle, setRowFetchStyle] = useState(rowStyles.fetchReset)
     const colorChangeDebounceTimer = useRef(setTimeout(() => {}, 0))
 
     async function changeBrightness(device: string, model: string, inputBrightness: number) {
@@ -72,16 +142,38 @@ export function TableOfLights(props: any) {
             },
             body: JSON.stringify(commandBody)
         })
-
-        // TODO: Figure out a way to deal with rate limiting, show visually.
         if (response.status === 200) {
             setBrightness(inputBrightness)
+            updateIllumination(inputBrightness)
+            flashRowOnSuccess()
+            setRateLimited(false)
+        }
+        else if (response.status === 429) {
+            console.log(response)
+            setBrightness(light.status.brightness)
+            updateIllumination(inputBrightness)
+            flashRowOnFailure()
+            setRateLimited(true)
         }
         else {
             console.log("Hmm, something went wrong.", response)
         }
     }
 
+    function flashRowOnSuccess() {
+        setRowFetchStyle(rowStyles.fetchSuccess)
+        setTimeout(() => {
+            setRowFetchStyle(rowStyles.fetchReset)
+        }, 1000)
+    }
+
+    function flashRowOnFailure() {
+        setRowFetchStyle(rowStyles.fetchFailure)
+    }
+
+    function updateIllumination(brightness: number) {
+        brightness > 0 ? setIlluminating(true) : setIlluminating(false)
+    }
 
     // Sends a debounced request to the server to change the color of the light.
     async function changeColor(device: string, model: string, inputColor: string) {
@@ -109,7 +201,17 @@ export function TableOfLights(props: any) {
                 body: JSON.stringify(commandBody)
             })
             if (response.status === 200) {
-                console.log("Color changed successfully.")
+                setColor(inputColor)
+                // Sending black as a color request to their API turns the light off lol.
+                inputColor === "#000000" ? updateIllumination(0) : updateIllumination(brightness)
+                flashRowOnSuccess()
+                setRateLimited(false)
+            }
+            else if (response.status === 429) {
+                setColor(color)
+                flashRowOnFailure()
+                setRateLimited(true)
+                console.log("Rate limit exceeded.")
             }
             else {
                 console.log("Hmm, something went wrong.", response)
@@ -117,88 +219,64 @@ export function TableOfLights(props: any) {
         }
     }
 
-    const rowsSorted = props.data.sort((a, b) => {
-        return a.details.deviceName.toLowerCase().localeCompare(b.details.deviceName.toLowerCase())
-    })
-    const rows = rowsSorted.map((light: goveeDeviceWithState) => {
-        let currentColor = ""
-        const lightStatus = light.status.online ?
-            <Badge color="green" variant="filled"> Online </Badge> :
-            <Badge color="red" variant="filled"> Offline </Badge>
-
+    useEffect(() => {
         // This can happen when setting the lights to a bulb color and brightness, rather than an RGB one.
         if (light.status.colorTem) {
-            currentColor = "FFFFFF"
+            setColor("#ffffff")
         }
         else if (light.status.color) {
             const red = light.status.color.r
             const green = light.status.color.g
             const blue = light.status.color.b
-            currentColor = rgbToHex(red, green, blue)
+            setColor(rgbToHex(red, green, blue))
         }
-
-        return (
-            <tr key={light.id}>
-                <td>
-                    <Text component="span"
-                          align="center"
-                          variant="gradient"
-                          gradient={{ from: 'indigo', to: 'pink', deg: 45 }}
-                          size="xl"
-                          weight={700}
-                          style={{ fontFamily: 'Greycliff CF, sans-serif' }}>
-                        {light.details.deviceName}
-                    </Text>
-                </td>
-                <td>{light.details.model}</td>
-                <td>{lightStatus}</td>
-                <td>
-                    <ColorPicker
-                        fullWidth={true}
-                        value={currentColor}
-                        size="sm"
-                        // According to Mantine docs, onChangeEnd is supposed to exist on ColorPicker element but doesn't,
-                        // so we'll need to debounce in changeColor method.
-                        onChange={(color) => changeColor(light.id, light.details.model, color)}
-                        className={classes["control-surface"]}
-                    />
-                    <Slider
-                        radius="xl"
-                        size="xl"
-                        color="grape"
-                        precision={0}
-                        defaultValue={light.status.brightness}
-                        className={classes["control-surface"]}
-                        onChangeEnd={(currentValue) => changeBrightness(light.id, light.details.model, currentValue)}
-                        marks={[
-                            { value: 10, label: "Dim" },
-                            { value: 50, label: "Moody" },
-                            { value: 90, label: "Bright" },
-                        ]}
-                    />
-                </td>
-            </tr>
-        )
-    })
+    },[])
 
     return (
-        <Table sx={{ maxWidth: '1000px' }}
-               verticalSpacing="xs"
-               striped={true}
-               horizontalSpacing="xs"
-               highlightOnHover={true}
-               className="table-lights-status">
-            <thead>
-                <tr>
-                    <th>Light Location</th>
-                    <th>Model</th>
-                    <th>Status</th>
-                    <th>Color and Brightness</th>
-                </tr>
-            </thead>
-            <tbody>
-                {rows}
-            </tbody>
-        </Table>
+        <tr style={rowFetchStyle}>
+            <td>
+                <Text component="span"
+                      align="center"
+                      variant="gradient"
+                      gradient={{ from: 'indigo', to: 'pink', deg: 45 }}
+                      size="xl"
+                      weight={700}
+                      style={{ fontFamily: 'Greycliff CF, sans-serif' }}>
+                    {light.details.deviceName}
+                </Text>
+            </td>
+            <td>{light.details.model}</td>
+            <td className="status-td">
+                <BadgeNetworkStatus online={online}/>
+                <BadgeIlluminationStatus illuminating={illuminating} rateLimited={rateLimited}/>
+            </td>
+            <td>
+                <ColorPicker
+                    fullWidth={true}
+                    value={color}
+                    size="sm"
+                    // According to Mantine docs, onChangeEnd is supposed to exist on ColorPicker element but doesn't,
+                    // so we'll need to debounce in changeColor method.
+                    onChange={(inputColor) => changeColor(light.id, light.details.model, inputColor)}
+                    style={rowStyles.controlSurface}
+                />
+                <Slider
+                    size="xl"
+                    thumbSize={32}
+                    step={10}
+                    thumbChildren={<BulbIcon/>}
+                    color="dark"
+                    precision={0}
+                    defaultValue={light.status.powerState === "on" ? light.status.brightness : 0}
+                    style={rowStyles.controlSurface}
+                    onChangeEnd={(currentValue) => changeBrightness(light.id, light.details.model, currentValue)}
+                    marks={[
+                        { value: 10, label: "Dim" },
+                        { value: 50, label: "Moody" },
+                        { value: 90, label: "Bright" },
+                    ]}
+                />
+            </td>
+        </tr>
     )
 }
