c4c54114ad5b6a20ab743f575aeccf8b5657c442
7MinutesDead-Git <7minutesdead@gmail.com>
2022-10-13 05:36:56 -0500

    Fix server URL including duplicated forward slash when deployed to netlify

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 let baseURL = process.env.REACT_APP_SERVER_URL
 if (process.env.NODE_ENV === "development") {
-    baseURL = "http://localhost:8080"
+    baseURL = "http://localhost:8080/"
 }
-export const devicesURL = `${baseURL}/devices`
+export const devicesURL = `${baseURL}devices`
 export const stateURL = `${devicesURL}/state`
