2cc683bf4c93f253088ff5118d1c395bd6c2e30f
7MinutesDead-Git <7minutesdead@gmail.com>
2022-10-20 07:42:22 -0500

    Add config for swapping between production and development websocket URLs

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,16 @@
 let baseURL = process.env.REACT_APP_SERVER_URL
+let baseWebSocketURL = process.env.REACT_APP_SERVER_SOCKET
 if (process.env.NODE_ENV === "development") {
     baseURL = "http://localhost:8080/"
+    baseWebSocketURL = "ws://localhost:8080"
 }
 
+export const websocketURL = baseWebSocketURL
 export const devicesURL = `${baseURL}devices`
 export const stateURL = `${devicesURL}/state`
-export const rateLimitExpireURL = `${baseURL}rate-limit`
+export const rateLimitExpireURL = `${devicesURL}/rate-limit`
 export enum intervals {
+    twoSeconds = 2000,
     fiveSeconds = 5000,
     tenSeconds = 10000,
     thirtySeconds = 30000,
