bbefc6747256d6449a2ea801b2d49dd650677003
7MinutesDead-Git <7minutesdead@gmail.com>
2022-10-20 07:38:48 -0500

    Create utility static websocket class for broadcasting UI changes

---
+++ src/api/websocket-utilities.ts
@@ -0,0 +1,23 @@
+import { websocketURL } from "../config"
+
+export const multiplayer = {
+    client: new WebSocket(websocketURL!),
+
+    broadcastBrightnessChange(id: string, sliderValue: number): void {
+        const message = JSON.stringify({
+            device: id,
+            type: "brightness",
+            value: sliderValue,
+        })
+        this.client.send(message)
+    },
+
+    broadcastColorChange(id: string, colorValue: string): void {
+        const message = JSON.stringify({
+            device: id,
+            type: "color",
+            value: colorValue,
+        })
+        this.client.send(message)
+    },
+}
