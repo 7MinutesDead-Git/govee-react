809f4a7d450cff8ea8bb68dc59e59d4bd4bfadab
7MinutesDead-Git <7minutesdead@gmail.com>
2022-10-29 15:29:10 -0500

    Add functions for lerping between color values (hex strings and RGB objects)

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,46 @@
-export function rgbToHex(r: number, g: number, b: number) {
+import { rgbColor } from "../interfaces/interfaces";
+
+export function rgbToHex(color: rgbColor | undefined) {
+    if (!color) {
+        return "#000000"
+    }
+    const { r, g, b } = color
+
     return "#" + [r, g, b].map(x => {
         const hex = x.toString(16)
         return hex.length === 1 ? "0" + hex : hex
     }).join("")
+}
+
+// Parse a hex color string into an RGB object.
+export function hexToRGB(hex: string): rgbColor {
+    return {
+        "r": parseInt(hex.slice(1, 3), 16),
+        "g": parseInt(hex.slice(3, 5), 16),
+        "b": parseInt(hex.slice(5, 7), 16)
+    }
+}
+
+// Linearly interpolate between two values based on a distance between the two.
+function lerp(start: number, end: number, distance: number): number {
+    const q = 1 - distance
+    return Math.round(start * q + end * distance)
+}
+
+// Linearly interpolate between two colors as RGB objects.
+export function lerpColorRGB(start: rgbColor, end: rgbColor, distance: number): rgbColor {
+    return {
+        r: lerp(start.r, end.r, distance),
+        g: lerp(start.g, end.g, distance),
+        b: lerp(start.b, end.b, distance)
+    }
+}
+
+
+// Linearly interpolate between two hex color values.
+export function lerpColorHex(start: string, end: string, distance: number) {
+    const startRGB = hexToRGB(start)
+    const endRGB = hexToRGB(end)
+    const lerpRGB = lerpColorRGB(startRGB, endRGB, distance)
+    return rgbToHex(lerpRGB)
 }
