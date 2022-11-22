e6609522e459dffb05be19e86fac82c7d7722ff4
7MinutesDead-Git <7minutesdead@gmail.com>
2022-11-22 04:09:00 -0600

    Remove unused header animation setup

--- src/components/LightsHeader.tsx
+++ src/components/LightsHeader.tsx
@@ -1,11 +1,11 @@
 import {Header, Center} from "@mantine/core"
-import React, {useEffect, useRef} from "react"
+import { CSSProperties, ReactNode } from "react"
 import { HeaderLinkButton } from "./HeaderLinkButton"
 import { SocialIcon } from "react-social-icons"
 
 
 interface LightsHeaderProps {
-    children?: React.ReactNode,
+    children?: ReactNode,
     color?: string,
 }
 
@@ -26,32 +26,15 @@ const styles = {
         backgroundColor: "#121111",
         padding: "0 1rem",
         zIndex: 100,
-    } as React.CSSProperties,
+    } as CSSProperties,
 
     socialIcons: {
         height: 40,
         width: 40,
-    } as React.CSSProperties,
+    } as CSSProperties,
 }
 
-const initialEffectArrayWidth = Math.floor(window.innerWidth / 110)
-const emptyEffectButtonArray = Array(initialEffectArrayWidth).fill(null)
-
 export const LightsHeader = (props: LightsHeaderProps) => {
-    const [ animationButtons, setAnimationButtons ] = React.useState(emptyEffectButtonArray)
-    const [ effectArrayWidth, setEffectArrayWidth ] = React.useState(initialEffectArrayWidth)
-    const [ headerButtonAnimation, setHeaderButtonAnimation ] = React.useState("none")
-    const headerAnimationCompleteDelay = useRef(0)
-
-    // Updates the number of EffectButtons in the header when the window is resized.
-    useEffect(() => {
-        function handleResize() {
-            const newWidth = Math.floor(window.innerWidth / 110)
-            setEffectArrayWidth(newWidth)
-        }
-        window.addEventListener("resize", handleResize)
-        return () => window.removeEventListener("resize", handleResize)
-    })
 
     return (
         <Header
@@ -60,7 +43,7 @@ export const LightsHeader = (props: LightsHeaderProps) => {
             fixed={true}
             style={styles.lightsHeader}>
             <Center>
-                <HeaderLinkButton href="https://7minutes.dev/?referrer=govee" animation={headerButtonAnimation}>
+                <HeaderLinkButton href="https://7minutes.dev/?referrer=govee">
                     <SocialIcon
                         network=""
                         target="_blank"
@@ -70,7 +53,7 @@ export const LightsHeader = (props: LightsHeaderProps) => {
                         bgColor="transparent"/>
                     7minutes.dev
                 </HeaderLinkButton>
-                <HeaderLinkButton href="https://www.linkedin.com/in/alex-gulikers-28256a219/" animation={headerButtonAnimation}>
+                <HeaderLinkButton href="https://www.linkedin.com/in/alex-gulikers-28256a219/">
                     <SocialIcon
                         network="linkedin"
                         target="_blank"
@@ -80,7 +63,7 @@ export const LightsHeader = (props: LightsHeaderProps) => {
                         bgColor="transparent"/>
                     contact
                 </HeaderLinkButton>
-                <HeaderLinkButton href="https://github.com/7MinutesDead-Git/govee-react" animation={headerButtonAnimation}>
+                <HeaderLinkButton href="https://github.com/7MinutesDead-Git/govee-react">
                     <SocialIcon
                         network="github"
                         target="_blank"
@@ -90,9 +73,6 @@ export const LightsHeader = (props: LightsHeaderProps) => {
                         bgColor="transparent"/>
                     repo
                 </HeaderLinkButton>
-                <div className="hover-effect">
-                    { animationButtons }
-                </div>
             </Center>
             {props.children}
         </Header>
