a80cd2c32fd08997bcf06b7461283e37b7a66195
7MinutesDead-Git <7minutesdead@gmail.com>
2022-12-14 19:49:03 -0600

    Constants and durations refactor (...) - Extract magic numbers to constants object. - Remove redundant styleTimer from websocket effect. - Decrease color change debounce delay from 500ms to 100ms for more responsive mutation (thanks Govee!) - Increase speed of client-side linear interpolation of color changes from 60fps to 120fps (16.667ms -> 8.333ms)

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { durations, intervals } from "./utils/constants"
+
 let baseURL = process.env.REACT_APP_SERVER_URL
 let baseWebSocketURL = process.env.REACT_APP_SERVER_SOCKET
 if (process.env.NODE_ENV === "development") {
@@ -11,28 +13,12 @@ export const stateURL = `${devicesURL}/state`
 export const rateLimitExpireURL = `${devicesURL}/rate-limit`
 export const loginURL = `${baseURL}auth`
 
-export enum intervals {
-    twoSeconds = 2000,
-    fiveSeconds = 5000,
-    tenSeconds = 10000,
-    thirtySeconds = 30000,
-    oneMinute = 60000,
-    fiveMinutes = 300000,
-    tenMinutes = 600000,
-}
-enum refreshRates {
-    fps200 = 5,
-    fps144 = 6.944,
-    fps120 = 8.333,
-    fps90 = 11.111,
-    fps60 = 16.667,
-    fps30 = 33.333,
-}
+
 export class NetworkConfig {
     // 1 will be instantaneous, 0.5 is fast smoothing, 0.1 will be slow, 0.01 will be very slow
     static lerpScale = 0.25
     // How quickly a new value is lerped to the current value, in milliseconds. 16.7ms is 60fps
-    static lerpUpdateRate = refreshRates.fps90
+    static lerpUpdateRate = durations.fps120
     // How quickly commands from the server are processed by the client, in milliseconds.
     static socketUpdateRate = 50
 }
