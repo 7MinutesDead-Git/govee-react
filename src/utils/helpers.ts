45b178588d2779ccab0d3f88cd622c1bb15b3433
7MinutesDead-Git <7minutesdead@gmail.com>
2022-12-21 12:20:08 -0600

    Add type guard to check if a value is an rgbColor object

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,11 @@
 // Clamps a value between a minimum and maximum value.
+import { rgbColor } from "../interfaces/interfaces";
+
 export function clamp(target: number, min: number, max: number) {
     return Math.max(min, Math.min(target, max))
+}
+
+// Type guard to check if a value is an rgbColor object.
+export function isRgbColor(value: string | rgbColor | number): value is rgbColor {
+    return (value as rgbColor).r !== undefined
 }
