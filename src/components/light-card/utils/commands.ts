25a7a28f8cd5b82a7f3c8c1c673b1328b268953e
7MinutesDead-Git <7minutesdead@gmail.com>
2022-12-28 22:13:08 -0600

    Reduce complexity of sendLightCommand function by extracting out responsibilities (...) - Add new function determineCommandName - Add new function normalizeValue - Add new function inColorTemperatureRange

--- src/components/light-card/utils/commands.ts
+++ src/components/light-card/utils/commands.ts
@@ -1,28 +1,53 @@
 import { devicesURL } from "../../../config"
 import { goveeDeviceWithState, rgbColor } from "../../../interfaces/interfaces"
 import { hexToRGB } from "../../../utils/colorFunctions"
-import { clamp } from "../../../utils/helpers"
+import { clamp, isRgbColor } from "../../../utils/helpers"
+import { temperatures } from "../controls/TemperatureSlider"
 
-// Send the given light command to the server. Color or brightness can be changed.
-// Brightness accepts a number or string value between 0 and 100.
-// Color accepts a hex value as string, or an object with r, g, and b properties.
-export async function sendLightCommand(light: goveeDeviceWithState, value: string | rgbColor | number) {
-    let commandName = null
 
+function inColorTemperatureRange(value: number): boolean {
+    return value >= temperatures.min && value <= temperatures.max
+}
+
+function normalizeValue(value: string | rgbColor | number): string | rgbColor | number {
     // Convert hex color input to rgb.
     if (typeof value === "string" && value.startsWith("#")) {
         value = hexToRGB(value)
-        commandName = "color"
     }
     // Explicitly convert number as string input to number type, since we can also accept colors as strings.
     if (!isNaN(Number(value))) {
         value = Number(value)
     }
-    // A number value means this is a brightness command, so clamp brightness within acceptable range.
+    // A valid number value means this is either a brightness command or a color temperature command.
     if (typeof value === "number") {
-        value = clamp(value, 0, 100)
-        commandName = "brightness"
+        if (inColorTemperatureRange(value)) {
+            return value
+        }
+        else {
+            return clamp(value, 0, 100)
+        }
     }
+    return value
+}
+
+function determineCommandName(value: string | rgbColor | number): string | null {
+    if (typeof value === "string" || isRgbColor(value)) {
+        return "color"
+    }
+    // A number value means this is a brightness command, so clamp brightness within acceptable range.
+    if (inColorTemperatureRange(value)) {
+        return "colorTem"
+    }
+    return "brightness"
+}
+
+// Send the given light command to the server. Color, brightness or temperature can be changed.
+// Brightness accepts a number or string value between 0 and 100.
+// Color accepts a hex value as string, or an object with r, g, and b properties.
+// Temperature accepts a number between temperatures.min and temperatures.max.
+export async function sendLightCommand(light: goveeDeviceWithState, value: string | rgbColor | number) {
+    value = normalizeValue(value)
+    const commandName = determineCommandName(value)
 
     if (!commandName) {
         throw new Error(`Invalid command value of type ${typeof value} sent to light ${light.details.deviceName}: ${value}`)
