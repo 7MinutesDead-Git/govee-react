4b9f4a0c7fcaf977414d692c562f6cf45665561d
7MinutesDead-Git <7minutesdead@gmail.com>
2023-01-31 11:32:04 -0600

    Add unit test for Badge component offline status

--- src/components/Badges.test.tsx
+++ src/components/Badges.test.tsx
@@ -6,3 +6,9 @@ test('Network badge component shows online text when online', () => {
     const badgeElement = getByText("Online")
     expect(badgeElement).toBeInTheDocument()
 })
+
+test('Network badge component shows offline text when offline', () => {
+    const { getByText } = render(<BadgeNetworkStatus online={false}/>)
+    const badgeElement = getByText("Offline")
+    expect(badgeElement).toBeInTheDocument()
+})
