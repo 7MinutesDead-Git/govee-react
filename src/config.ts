370ecbef9512cfb5b639fbae3c2c99b6e16dbe49
7MinutesDead-Git <7minutesdead@gmail.com>
2023-03-28 14:09:56 -0500

    Update process.env to import.meta.env per Vite standards (...) - Add additional explanations for what the localhost URLs are

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,14 @@
 import { durations, intervals } from "./utils/constants"
 
-let baseURL = process.env.REACT_APP_SERVER_URL
-let baseWebSocketURL = process.env.REACT_APP_SERVER_SOCKET
-if (process.env.NODE_ENV === "development") {
+// https://github.com/vitejs/vite/issues/9539#issuecomment-1206301266
+let baseURL = import.meta.env.VITE_SERVER_URL
+let baseWebSocketURL = import.meta.env.VITE_SERVER_SOCKET
+// https://vitejs.dev/guide/env-and-mode.html#env-variables-and-modes
+if (import.meta.env.DEV) {
+    // Note this is for pointing to the backend server in development,
+    // (as in, the backend server is on localhost 8080).
+    // This is NOT for the frontend React/Vite server that this code you're reading now
+    // is a part of, that is managed from .env.
     baseURL = "http://localhost:8080/"
     baseWebSocketURL = "ws://localhost:8080"
 }
